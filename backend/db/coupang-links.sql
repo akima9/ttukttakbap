07c9c0983d9fc86b6226fa4ee327a30b311926f1
devGiYong <akima9a@gmail.com>
2026-07-17 15:43:40 +0900

    feat(monetization): 재료 68종 쿠팡 파트너스 딥링크 반영
    - backend/db/coupang-links.sql 추가 (재료 id 기준 UPDATE 68줄)
    - DEPLOY.md 시드 import 절차에 coupang-links.sql import 한 줄 추가

---
+++ backend/db/coupang-links.sql
@@ -0,0 +1,72 @@
+-- 쿠팡 파트너스 딥링크 (재료 id 기준 UPDATE)
+-- dummy-data.sql 시드 import 뒤에 함께 import한다. 워크시트 매핑에서 자동 생성.
+SET NAMES utf8mb4;
+
+UPDATE ingredient SET coupang_url = 'https://link.coupang.com/a/frWc5cIEX6' WHERE id = 1;   -- 양파
+UPDATE ingredient SET coupang_url = 'https://link.coupang.com/a/frWjNzoUhw' WHERE id = 2;   -- 대파
+UPDATE ingredient SET coupang_url = 'https://link.coupang.com/a/frWoMtxAAu' WHERE id = 3;   -- 마늘
+UPDATE ingredient SET coupang_url = 'https://link.coupang.com/a/frYsL5i4ia' WHERE id = 4;   -- 소금
+UPDATE ingredient SET coupang_url = 'https://link.coupang.com/a/frYu9aaacK' WHERE id = 5;   -- 간장
+UPDATE ingredient SET coupang_url = 'https://link.coupang.com/a/frYwIoUBO1' WHERE id = 6;   -- 참기름
+UPDATE ingredient SET coupang_url = 'https://link.coupang.com/a/frYydIX1d7' WHERE id = 7;   -- 설탕
+UPDATE ingredient SET coupang_url = 'https://link.coupang.com/a/frYC4SqTXU' WHERE id = 8;   -- 식용유
+UPDATE ingredient SET coupang_url = 'https://link.coupang.com/a/frYFqNK46e' WHERE id = 9;   -- 달걀
+UPDATE ingredient SET coupang_url = 'https://link.coupang.com/a/frYKGZHwwS' WHERE id = 10;   -- 당근
+UPDATE ingredient SET coupang_url = 'https://link.coupang.com/a/frYNjHFG9c' WHERE id = 11;   -- 고추장
+UPDATE ingredient SET coupang_url = 'https://link.coupang.com/a/frYPbQwiGq' WHERE id = 12;   -- 고춧가루
+UPDATE ingredient SET coupang_url = 'https://link.coupang.com/a/frYRwDRpEi' WHERE id = 13;   -- 두부
+UPDATE ingredient SET coupang_url = 'https://link.coupang.com/a/frY7qEJI1A' WHERE id = 14;   -- 된장
+UPDATE ingredient SET coupang_url = 'https://link.coupang.com/a/frY9II3EDA' WHERE id = 15;   -- 애호박
+UPDATE ingredient SET coupang_url = 'https://link.coupang.com/a/frZbgjsLf2' WHERE id = 16;   -- 배추김치
+UPDATE ingredient SET coupang_url = 'https://link.coupang.com/a/frZc67THb2' WHERE id = 17;   -- 돼지고기 삼겹살
+UPDATE ingredient SET coupang_url = 'https://link.coupang.com/a/frZlo0zd92' WHERE id = 18;   -- 당면
+UPDATE ingredient SET coupang_url = 'https://link.coupang.com/a/frZoaKawj6' WHERE id = 19;   -- 소고기 채끝
+UPDATE ingredient SET coupang_url = 'https://link.coupang.com/a/frZp0YuErQ' WHERE id = 20;   -- 시금치
+UPDATE ingredient SET coupang_url = 'https://link.coupang.com/a/fsbGcas3Nc' WHERE id = 21;   -- 표고버섯
+UPDATE ingredient SET coupang_url = 'https://link.coupang.com/a/fsbIqm8diK' WHERE id = 22;   -- 소고기 불고기감
+UPDATE ingredient SET coupang_url = 'https://link.coupang.com/a/fsbKxrwJ7Q' WHERE id = 23;   -- 배
+UPDATE ingredient SET coupang_url = 'https://link.coupang.com/a/fsbNcIkqCO' WHERE id = 24;   -- 맛술
+UPDATE ingredient SET coupang_url = 'https://link.coupang.com/a/fsbOVdpof6' WHERE id = 25;   -- 순두부
+UPDATE ingredient SET coupang_url = 'https://link.coupang.com/a/fsbRdXyCU8' WHERE id = 26;   -- 새우젓
+UPDATE ingredient SET coupang_url = 'https://link.coupang.com/a/fsbS74Y6PA' WHERE id = 27;   -- 쌀
+UPDATE ingredient SET coupang_url = 'https://link.coupang.com/a/fsbUAcljbx' WHERE id = 28;   -- 콩나물
+UPDATE ingredient SET coupang_url = 'https://link.coupang.com/a/fsbXjEWnx6' WHERE id = 29;   -- 고사리
+UPDATE ingredient SET coupang_url = 'https://link.coupang.com/a/fsbYUB2o32' WHERE id = 30;   -- 가래떡(떡볶이용)
+UPDATE ingredient SET coupang_url = 'https://link.coupang.com/a/fsb0VgeSsu' WHERE id = 31;   -- 어묵
+UPDATE ingredient SET coupang_url = 'https://link.coupang.com/a/fsb3mZ72Dk' WHERE id = 32;   -- 돼지갈비
+UPDATE ingredient SET coupang_url = 'https://link.coupang.com/a/fsb6KESNbM' WHERE id = 33;   -- 무
+UPDATE ingredient SET coupang_url = 'https://link.coupang.com/a/fsb8ajltmu' WHERE id = 34;   -- 생강
+UPDATE ingredient SET coupang_url = 'https://link.coupang.com/a/fsb9zrfkQK' WHERE id = 35;   -- 돼지고기 목살
+UPDATE ingredient SET coupang_url = 'https://link.coupang.com/a/fscchuac6m' WHERE id = 36;   -- 국간장
+UPDATE ingredient SET coupang_url = 'https://link.coupang.com/a/fscdWZw5OS' WHERE id = 37;   -- 건미역
+UPDATE ingredient SET coupang_url = 'https://link.coupang.com/a/fscf5QDbm8' WHERE id = 38;   -- 소고기 양지
+UPDATE ingredient SET coupang_url = 'https://link.coupang.com/a/fscixbhC0a' WHERE id = 39;   -- 스팸
+UPDATE ingredient SET coupang_url = 'https://link.coupang.com/a/fscj7rJ4Im' WHERE id = 40;   -- 비엔나 소시지
+UPDATE ingredient SET coupang_url = 'https://link.coupang.com/a/fsclTGj8zQ' WHERE id = 41;   -- 라면사리
+UPDATE ingredient SET coupang_url = 'https://link.coupang.com/a/fscoo8Prky' WHERE id = 42;   -- 슬라이스 치즈
+UPDATE ingredient SET coupang_url = 'https://link.coupang.com/a/fscp9Pc376' WHERE id = 43;   -- 떡국떡
+UPDATE ingredient SET coupang_url = 'https://link.coupang.com/a/fscrKv2lRk' WHERE id = 44;   -- 김
+UPDATE ingredient SET coupang_url = 'https://link.coupang.com/a/fsctxHoXF6' WHERE id = 45;   -- 소면
+UPDATE ingredient SET coupang_url = 'https://link.coupang.com/a/fscu6uZe8a' WHERE id = 46;   -- 국물용 멸치
+UPDATE ingredient SET coupang_url = 'https://link.coupang.com/a/fscxyc04l2' WHERE id = 47;   -- 오이
+UPDATE ingredient SET coupang_url = 'https://link.coupang.com/a/fscy4fb9SC' WHERE id = 48;   -- 식초
+UPDATE ingredient SET coupang_url = 'https://link.coupang.com/a/fscAyaa8cu' WHERE id = 49;   -- 칼국수면
+UPDATE ingredient SET coupang_url = 'https://link.coupang.com/a/fscEjX9V0K' WHERE id = 50;   -- 감자
+UPDATE ingredient SET coupang_url = 'https://link.coupang.com/a/fscFHMjj2q' WHERE id = 51;   -- 바지락
+UPDATE ingredient SET coupang_url = 'https://link.coupang.com/a/fscIbjHm5A' WHERE id = 52;   -- 물엿
+UPDATE ingredient SET coupang_url = 'https://link.coupang.com/a/fscJJb2F88' WHERE id = 53;   -- 통깨
+UPDATE ingredient SET coupang_url = 'https://link.coupang.com/a/fscLwjfzWu' WHERE id = 54;   -- 김밥용 김
+UPDATE ingredient SET coupang_url = 'https://link.coupang.com/a/fscNmkMOBM' WHERE id = 55;   -- 단무지
+UPDATE ingredient SET coupang_url = 'https://link.coupang.com/a/fscPD3PL4u' WHERE id = 56;   -- 맛살
+UPDATE ingredient SET coupang_url = 'https://link.coupang.com/a/fscRaYhIGq' WHERE id = 57;   -- 우엉조림
+UPDATE ingredient SET coupang_url = 'https://link.coupang.com/a/fscSNmBMU8' WHERE id = 58;   -- 밀가루
+UPDATE ingredient SET coupang_url = 'https://link.coupang.com/a/fscVLnY7Sm' WHERE id = 59;   -- 드라이이스트
+UPDATE ingredient SET coupang_url = 'https://link.coupang.com/a/fscXFpNntA' WHERE id = 60;   -- 흑설탕
+UPDATE ingredient SET coupang_url = 'https://link.coupang.com/a/fsc0riovtI' WHERE id = 61;   -- 땅콩분태
+UPDATE ingredient SET coupang_url = 'https://link.coupang.com/a/fsc1TpEHW8' WHERE id = 62;   -- 계핏가루
+UPDATE ingredient SET coupang_url = 'https://link.coupang.com/a/fsc3rJDnyu' WHERE id = 63;   -- 부침가루
+UPDATE ingredient SET coupang_url = 'https://link.coupang.com/a/fsddf6Qi1A' WHERE id = 64;   -- 고구마
+UPDATE ingredient SET coupang_url = 'https://link.coupang.com/a/fsdj0waOpE' WHERE id = 65;   -- 닭다리살
+UPDATE ingredient SET coupang_url = 'https://link.coupang.com/a/fsdlyvGMCq' WHERE id = 66;   -- 양배추
+UPDATE ingredient SET coupang_url = 'https://link.coupang.com/a/fsdnF0NbHw' WHERE id = 67;   -- 깻잎
+UPDATE ingredient SET coupang_url = 'https://link.coupang.com/a/fsdpBsvLiK' WHERE id = 68;   -- 골뱅이 통조림
